9bb9af48f77693333b5084a48398ffaba8f03244
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2026-08-09 13:12:51 -0500

    Correct punctuation removal function implementation
    Fix indentation and ensure punctuation removal works correctly.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 punctuations = '"!@#$%^&*()_+=-:{}|[];<>?,./"'
 
 def punctuation_removal(my_string):
+  no_punctuation = ""
   for char in my_string:
     if char not in punctuations:
       no_punctuation = no_punctuation + char
