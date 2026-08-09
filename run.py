23747cfffd452f8b25b5f1f031671036498962d3
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2026-08-09 13:11:49 -0500

    Refactor punctuation removal into a function

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 punctuations = '"!@#$%^&*()_+=-:{}|[];<>?,./"'
-my_string = "Hello!!, he said -- and went."
-no_punctuation = ""
-for char in my_string:
-  if char not in punctuations:
-    no_punctuation = no_punctuation + char
-    print(no_punctuation)
+
+def punctuation_removal(my_string):
+  for char in my_string:
+    if char not in punctuations:
+      no_punctuation = no_punctuation + char
+      print(no_punctuation)
+
+punctuation_removal("Hello!!, he said -- and went.")
